bb1c60d76455c166aea9be8cd3b68f9e238de274
Kirenga Danick <ndatinyakdanick@gmail.com>
2024-01-09 12:07:41 +0200

    Before rebase

--- vite-project/backend/server.js
+++ vite-project/backend/server.js
@@ -3,6 +3,10 @@ const express = require('express')
 //initialise the app
 const app = express()
 
-app.listen(3000, () => {
+app.get('/', (req, res) => {
+    res.json({"users": ["user one", "User two", "user three"]})
+})
+
+app.listen(5000, () => {
     console.log('Listening at port 3000')
 })
