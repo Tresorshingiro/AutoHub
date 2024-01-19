629dfba79ba51dd019b7ca468b627468bd8f0a9a
Kirenga Danick <ndatinyakdanick@gmail.com>
2024-01-20 00:20:24 +0200

    Reception can now create a vehicle

--- vite-project/frontend/src/assets/components/receptionForm.jsx
+++ vite-project/frontend/src/assets/components/receptionForm.jsx
@@ -3,106 +3,133 @@ import '../../App.css';
 
 const ReceptionForm = () => {  
     // Define vehicleInfo state using the useState hook
-    const [vehicleInfo, setVehicleInfo] = useState({
-      owner: '',
-      brand: '',
-      plate: '',
-      insurance: '',
-      telephone: '',
-      email: '',
-      description:''
-    });
-
-    // Define a function to handle changes in the form
-    const handleChange = (e) => {
-    // Update the corresponding property in vehicleInfo state
-    setVehicleInfo({
-      ...vehicleInfo,
-      [e.target.name]: e.target.value,
-    });
-  };
+    const [owner, setOwner] = useState('')
+    const [brand, setBrand] = useState('')
+    const [plate, setPlate] = useState('')
+    const [insurance, setInsurance] = useState('')
+    const [telephone, setTelephone] = useState('')
+    const [email, setEmail] = useState('')
+    const [description, setDescription] = useState('')
+    const [error, setError] = useState(null);
 
   // Define a function to handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
     // Perform actions with vehicleInfo data, e.g., send it to a server
-    console.log('Submitted Vehicle Info:', vehicleInfo);
+    const vehicle = { owner, brand, plate, insurance, telephone, email, description }
+
+    const response = await fetch('http://localhost:3000/api/vehicles/', {
+      method: 'POST',
+      body: JSON.stringify({owner, brand, plate, insurance, telephone, email, description}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if(!response.ok) {
+      console.error('Error:', response.statusText);
+      const errorData = await response.json(); // If error response contains JSON data
+      console.error('Error Details:', errorData);
+      setError(errorData);
+    }
+    if(response.ok) {
+      const json = await response.json()
+
+      setOwner('')
+      setBrand('')
+      setPlate('')
+      setInsurance('')
+      setTelephone('')
+      setEmail('')
+      setDescription('')
+      setError(null)
+      console.log('new Vehicle added', json)
+    }
   }
 
   return(
     <form onSubmit={handleSubmit}>
         <label>
         <input 
-        type="text" 
-        name="owner" 
-        placeholder='Vehicle Owner'
-        className='row'
-        value={vehicleInfo.owner} 
-        onChange={handleChange}/>
+          type="text" 
+          name="owner" 
+          placeholder='Vehicle Owner'
+          className='row'
+          onChange={(e) => setOwner(e.target.value)}
+          value={owner}
+        />
         </label>
 
         <label>
         <input 
-        type="text" 
-        name="brand" 
-        placeholder='Vehicle Brand'
-        className='row'
-        value={vehicleInfo.brand} 
-        onChange={handleChange}/>
+          type="text" 
+          name="brand" 
+          placeholder='Vehicle Brand'
+          className='row'
+          onChange={(e) => setBrand(e.target.value)}
+          value={brand}
+        />
         </label>
 
         <label>
         <input 
-        type="text" 
-        name="plateNo" 
-        placeholder='Plate No'
-        className='row' 
-        value={vehicleInfo.plate} 
-        onChange={handleChange}/>
+          type="text" 
+          name="plate" 
+          placeholder='Plate No'
+          className='row' 
+          onChange={(e) => setPlate(e.target.value)}
+          value={plate}
+        />
         </label>
     
         <label>
         <input 
-        type="text" 
-        name="insurance" 
-        placeholder='Insurance'
-        className='row' 
-        value={vehicleInfo.insurance} 
-        onChange={handleChange}/>
+          type="text" 
+          name="insurance" 
+          placeholder='Insurance'
+          className='row' 
+          onChange={(e) => setInsurance(e.target.value)}
+          value={insurance}
+        />
         </label>
             
         <label>
         <input 
-        type="number" 
-        name="tel" 
-        placeholder='Telephone'
-        className='row' 
-        value={vehicleInfo.telephone} 
-        onChange={handleChange}/>
+          type="number" 
+          name="telephone" 
+          placeholder='Telephone'
+          className='row' 
+          onChange={(e) => setTelephone(e.target.value)}
+          value={telephone}
+        />
         </label>
 
         <label>
         <input 
-        type="text" 
-        name="email" 
-        placeholder='Email'
-        className='row' 
-        value={vehicleInfo.email} 
-        onChange={handleChange}/>
+          type="text" 
+          name="email" 
+          placeholder='Email'
+          className='row' 
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
         </label>
 
         <label>
         <input 
-        type="text" 
-        name="description" 
-        placeholder='Description'
-        className='row' 
-        value={vehicleInfo.description} 
-        onChange={handleChange}/>
+          type="text" 
+          name="description" 
+          placeholder='Description'
+          className='row' 
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
         </label>
 
         <br/>
-        <button type="submit" className='btn'>Save</button>
+        <button>Save</button>
+        {error && <div>{error.error}</div>}
       </form>
   );
 }
