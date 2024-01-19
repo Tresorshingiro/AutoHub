b2b7eb79f5e4467aa340ff3c7f8068cade496795
Kirenga Danick <ndatinyakdanick@gmail.com>
2024-01-19 17:21:47 +0200

    Refilling again

--- vite-project/frontend/src/assets/pages/Reception.jsx
+++ vite-project/frontend/src/assets/pages/Reception.jsx
@@ -1,8 +1,36 @@
 import React, { useState } from 'react';
 import '../../App.css';
+import ReceptionForm from '../components/receptionForm';
 
 const Reception = () => {
-  return ()
+
+  return (
+    <div className="container">
+      <section className="header">
+        <div className='lg'>
+          <h1>AutoHub</h1>
+        </div>
+        <div className='placeholder'>
+          <h3>Reception</h3>
+        </div>
+        <div className="user-icon">
+        <img src="/user.png" alt="User Icon" />
+        </div>
+      </section>
+    <div className="nav-links">
+      <div className="user-icon">
+        <img src="/user.png" alt="User Icon" />
+          <h2>Reception</h2>
+      </div>
+      <button className='button'>Car Registration</button>
+        <button className='button'>In-service Vehicles</button>
+        <button className='button'>Cleared Vehicles</button>
+      </div>
+    <div className='box'>
+      {<ReceptionForm />}
+    </div>
+ </div>
+);
 }
 
 export default Reception;
