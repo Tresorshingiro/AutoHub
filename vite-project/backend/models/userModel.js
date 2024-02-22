f957d41f4339e381fedd89850679719926965877
Kirenga Danick <ndatinyakdanick@gmail.com>
2024-02-22 19:49:34 +0200

    signup and hashing passwords works

--- vite-project/backend/models/userModel.js
+++ vite-project/backend/models/userModel.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     role: {
         type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
         required: true
     },
     email: {
@@ -16,5 +22,27 @@ const userSchema = new Schema({
         required: true
     },
 })
+
+// Static signup method
+userSchema.statics.signup = async function (role, username, email, password) {
+
+    const emailExist = await this.findOne({email})
+    const nameExist = await this.findOne({username})
+
+    if (emailExist) {
+        throw Error('Email already in use')
+    }
+    else if (nameExist) {
+        throw Error('Username already in use')
+    }
+    else {
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+    
+        const user = await this.create({ role, username, email, password: hash })
+    
+        return user;
+    }
+}
 
 module.exports = mongoose.model('User', userSchema)
