e8863b21151d476f4e08f91530eb57373e2ad17a
Kirenga Danick <ndatinyakdanick@gmail.com>
2024-03-15 18:07:13 +0200

    Revert "Added validator for email and password"
    This reverts commit 079665f68adc66288d79a6995570fd6946d47fef.

--- vite-project/backend/models/userModel.js
+++ vite-project/backend/models/userModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
-const validator = require('validator')
 
 const userSchema = new Schema({
     role: {
@@ -26,19 +25,6 @@ const userSchema = new Schema({
 
 // Static signup method
 userSchema.statics.signup = async function (role, username, email, password) {
-
-    // validation
-    if (!role || !username || !email || !password) {
-        throw Error('All fields must be complete')
-    }
-
-    if (!validator.isEmail(email)) {
-        throw Error('Email is not valid')
-    }
-
-    if (!validator.isStrongPassword(password)) {
-        throw Error('Password not strong enough')
-    }
 
     const emailExist = await this.findOne({email})
     const nameExist = await this.findOne({username})
