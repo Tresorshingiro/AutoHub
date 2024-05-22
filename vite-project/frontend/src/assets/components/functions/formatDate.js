ad23ea9fe2d12922cf2ce54b18acd6603093a67b
Kirenga Danick <ndatinyakdanick@gmail.com>
2024-05-22 12:39:59 +0200

    moved the formatdat function to frontend and created 2 more delete functions

---
+++ vite-project/frontend/src/assets/components/functions/formatDate.js
@@ -0,0 +1,14 @@
+// Function to format the date
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+  
+    return `${hours}:${minutes}:${seconds}_${day}/${month}/${year}`;
+};
+
+export default formatDate;
