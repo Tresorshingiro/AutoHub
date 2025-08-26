63f6461615c29337165947dd40b6097e614c76ad
Tresorshingiro <tresorshingiro26@gmail.com>
2025-08-26 13:02:48 +0200

    added patch

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cors({
     origin: ['https://autohub-sigma.vercel.app', 'http://localhost:5173'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
         'Content-Type', 
         'Authorization', 
